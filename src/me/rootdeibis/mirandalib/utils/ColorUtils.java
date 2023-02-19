4d0954a05db5ed31ac149a5acd8a67e1f2792610
rootDeibis <rootstudios@outlook.es>
2023-02-19 19:47:49 -0400

    modified

--- src/me/rootdeibis/mirandalib/utils/ColorUtils.java
+++ src/me/rootdeibis/mirandalib/utils/ColorUtils.java
@@ -1,8 +1,12 @@
 package me.rootdeibis.mirandalib.utils;
 
+import java.util.regex.Pattern;
+
 import org.bukkit.ChatColor;
 
 public class ColorUtils {   
+
+    private static String HEX_PATTERN = "^#[a-fA-F0-9]{6}";
     
     public static String apply(String colorString) {
         String parseHexColorsString = "";
@@ -18,5 +22,10 @@ public class ColorUtils {
         return ChatColor.translateAlternateColorCodes('&', parseHexColorsString);
     }
 
+
+
+    public static boolean hexValidate(String hex) {
+        return Pattern.compile(HEX_PATTERN).matcher(hex).find();
+    }
 
 }
