61bba8d3e3d5d8b1668b3104b4ccf59ad580db5c
rootDeibis <rootstudios@outlook.es>
2023-02-20 10:24:04 -0400

    Fixes

--- src/me/rootdeibis/mirandalib/managers/commands/MirandaCommand.java
+++ src/me/rootdeibis/mirandalib/managers/commands/MirandaCommand.java
@@ -8,31 +8,31 @@ import org.bukkit.command.CommandSender;
 
 public abstract class MirandaCommand extends Command {
 
+
     protected MirandaCommand(String name) {
         super(name);
     }
 
     protected MirandaCommand(String name, String description) {
         super(name);
-
         this.setDescription(description);
     }
 
     protected MirandaCommand(String name, String description, String... aliases) {
         super(name);
-
         this.setDescription(description);
         this.setAliases(Arrays.stream(aliases).collect(Collectors.toList()));
-
     }
 
     @Override
     public boolean execute(CommandSender arg0, String arg1, String[] arg2) {
-        return this.handle(arg0, arg1, this, arg2);
+        return this.handle(arg0, this, arg1, arg2);
     }
 
-    abstract boolean handle(CommandSender sender, String label, Command command, String[] args);
+    public abstract boolean handle(CommandSender sender, MirandaCommand command, String label, String[] arguments);
 
+
+    
     
     
 }
